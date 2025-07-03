d3d56752db26fe7ea31e675eda127fb23fbea04e
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-07-03 22:36:06 +0700

    Update config tailwindcss v4 and config theme for dark mode

---
+++ src/components/theme-control/ThemeControl.tsx
@@ -0,0 +1,14 @@
+import useDarkMode from 'hooks/useDarkMode'
+
+export default function ThemeControl() {
+  const { toggleDarkMode, isDarkMode } = useDarkMode()
+
+  return (
+    <div
+      onClick={toggleDarkMode}
+      className='relative w-14 h-8 rounded-3xl bg-slate-300 dark:bg-zinc-700 place--center cursor-pointer'
+    >
+      <div className={`absolute top-1 rounded-full w-6 h-6 bg-blue-500 ${isDarkMode ? 'left-1' : 'right-1'}`} />
+    </div>
+  )
+}
